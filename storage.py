1abd7131a046703580a2c442d326799073653503
Soy Miguel <magutierrezhijosa@gmail.com>
2026-03-15 20:23:49 +0100

    Creacion de storage.py para separar el guardado de data del main.py

---
+++ storage.py
@@ -0,0 +1,8 @@
+"""
+storage.py
+
+Modulo encargado dee guardar los resultados del scraping.
+Actualmente guarda en CSV, pero se puede extender facilmente
+para guardar JSON , base de datos, etc.
+
+"""

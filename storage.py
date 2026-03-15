ee48453543e8b8450b56c44aad2794d0d269a6e6
Soy Miguel <magutierrezhijosa@gmail.com>
2026-03-15 20:28:49 +0100

    Importaciones

--- storage.py
+++ storage.py
@@ -6,3 +6,12 @@ Actualmente guarda en CSV, pero se puede extender facilmente
 para guardar JSON , base de datos, etc.
 
 """
+
+# Importamos csv para escribir el resultado CSV
+import csv
+
+# Importamos os para comprobar si el archivo ya existe
+import os
+
+# Importamos las constanntes que necesitamos de config.py
+from config import CSV_FILE,CSV_HEADERS

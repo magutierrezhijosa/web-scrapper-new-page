f7c38b5ea54f181aa400dffa8abc5385c5e05c2f
Soy Miguel <magutierrezhijosa@gmail.com>
2026-03-16 14:44:07 +0100

    Declaracion de la funcion guardar_csv() que luego importaremos al main.py

--- storage.py
+++ storage.py
@@ -1,7 +1,7 @@
 """
 storage.py
 
-Modulo encargado dee guardar los resultados del scraping.
+Modulo encargado de guardar los resultados del scraping.
 Actualmente guarda en CSV, pero se puede extender facilmente
 para guardar JSON , base de datos, etc.
 
@@ -15,3 +15,33 @@ import os
 
 # Importamos las constanntes que necesitamos de config.py
 from config import CSV_FILE,CSV_HEADERS
+
+
+##########################################################
+#                     FUNCIONES                          #
+##########################################################
+
+def guardar_csv(resultados,modo="a"):
+
+    """
+    Guarda una lista de resultados en el archivo CSV .
+    Por defecto usa modo append ('a') para no sobreescribir datos anteriores.
+    Solo escribe el  encabezado si el archivo es nuevo
+    
+    """
+
+    # Comprobamos si el archivo ya existe para no escribir el encabezado dos veces 
+    archivo_existe = os.path.exists(CSV_FILE)
+
+    # Creamos el context manager (with)
+    with open(CSV_FILE, modo, newline="", encoding="utf-8") as f:
+
+        # Creamos el escritor para CSV y le agregamos los encabezados
+        writer = csv.DictWriter(f, fieldnames=CSV_HEADERS)
+
+        # Solo escribimos el encabezado si el archivo es nuevo
+        if not archivo_existe or modo=="w":
+
+            writer.writeheader()
+
+        

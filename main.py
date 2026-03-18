d4f68647a4bb06b4c531dfe210d1eda2910d5b19
Soy Miguel <magutierrezhijosa@gmail.com>
2026-03-18 14:17:28 +0100

    Archivo main.py terminado con la nueva organizacion por modulos

--- main.py
+++ main.py
@@ -15,10 +15,39 @@ from camoufox.sync_api import Camoufox
 from config import CAMOUFOX_PATH, ELEMENT_SCRAP_LOCATOR
 
 # Importamos las funciones de cookies.py
-from cookies import guardar_cookies, cargar_cookies
+from cookies import guardar_cookies, cargar_cookies , cookies_existen
 
 # Importamos las funciones de scraper.py
 from scraper import scrapear_todas_las_paginas
 
 # Importamos las funciones de storage.py
 from storage import borrar_csv
+
+##########################################################
+#                     MAIN                              #
+##########################################################
+
+def main():
+
+    with Camoufox(headless=False, executable_path=CAMOUFOX_PATH) as browser:
+
+        # Opciona: descomenta esta linea para empezar desde cero borrando el CSV anterior
+        # borrar_csv()
+
+        # Gestionamos la cookies - si existen las cargamos, si no las generamos
+        if cookies_existen():
+            page = cargar_cookies(browser)
+        else:
+            page = guardar_cookies(browser)
+
+        # Esperamos a que la pagina este lista antes de empezar
+        page.locator(ELEMENT_SCRAP_LOCATOR).first.wait_for(state="visible")
+
+        # Lanzamos el scraping 
+        resultados = scrapear_todas_las_paginas(page)
+
+        print(f"✅ Script finalizado. {len(resultados)} elementos guardados.")
+
+if __name__ == "__main__":
+
+    main()
